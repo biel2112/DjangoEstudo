a9301565b167a576bd7e5e18608cc78e9b9e540b
biel2112 <gabrielc_morais@hotmail.com>
2025-01-20 16:05:13 -0300

    Melhorando o template principal

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('''
-    <h1>Gabspace!</h1>
-    ''')
+    return render(request, 'galeria/index.html')
